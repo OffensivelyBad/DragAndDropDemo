02375e4ddff603852da7076126f8c115dd826ff9
SRoller <sroller@justfab.com>
2017-10-22 17:05:14 -0400

    Enables dragging postcards to the second collection view

---
+++ DragAndDropDemo/SavedPostcardCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  SavedPostcardCollectionViewCell.swift
+//  DragAndDropDemo
+//
+//  Created by Shawn Roller on 10/22/17.
+//  Copyright © 2017 Shawn Roller. All rights reserved.
+//
+
+import UIKit
+
+class SavedPostcardCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var postcardImage: UIImageView!
+    
+}
